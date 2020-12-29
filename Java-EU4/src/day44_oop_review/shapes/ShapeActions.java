12f9f8e8a73b014c179d677228cb5f8aa1aee411
user <user@192.168.1.113>
2020-12-30 00:49:39 +0400

    Day44

--- Java-EU4/src/day44_oop_review/shapes/ShapeActions.java
+++ Java-EU4/src/day44_oop_review/shapes/ShapeActions.java
@@ -1,15 +1,31 @@
 package day44_oop_review.shapes;
 import static day44_oop_review.shapes.ShapeManager.*;
+import static day44_oop_review.shapes.ShapeManager.drawSquare;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 public class ShapeActions {
 
 	public static void main(String[] args) {
 		
 		
-		Square mySquare=new Square();
-		
-		drawShape(mySquare);
-		
 		drawSquare((Square)buildShape("square"));
+		
+		Object rand=new Random();
+		System.out.println(((Random)rand).nextInt(5));
+		
+		System.out.println("---------------");
+		
+		List<Shape>myList=new ArrayList<>();
+		myList.add(new Shape());
+		myList.add(new Triangle());
+		myList.add(new Square());
+		myList.add(new Shape());
+		myList.add(new Triangle());
+		
+		drawShape(myList);
+		
 		
 		
 	}
