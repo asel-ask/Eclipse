21554300ef9d6073378e516cf4e1543f532dba2f
Asel05 <asel.9505@outlook.com>
2021-01-05 00:19:52 +0400

    eclipse

--- Java-EU4/src/assignments/assign_14/question14/OnSaleItem.java
+++ Java-EU4/src/assignments/assign_14/question14/OnSaleItem.java
@@ -1,5 +1,26 @@
 package assignments.assign_14.question14;
 
-public class OnSaleItem {
+public class OnSaleItem extends Item{
 
+	 private double discount;
+	 
+	public OnSaleItem(String name, int quantity, int catalogNumber,double price,double discount) {
+		super(name, quantity, catalogNumber,price-discount);
+		this.discount=discount;
+	}
+
+	 public double getDiscount() {
+		return discount;    
+	 }
+	
+	 public void setDiscount(double discount) { 
+		 this.discount=discount;
+	 }
+
+	@Override
+	public String toString() {
+		return "OnSaleItem {discount=" + discount + "%, price=" + getPrice() + ", name=" + getName() + "}";
+	}
+	 
+	 
 }
--- Java-EU4/src/assignments/assign_14/question14/TJMaxx.java
+++ Java-EU4/src/assignments/assign_14/question14/TJMaxx.java
@@ -1,5 +1,27 @@
 package assignments.assign_14.question14;
-
+import java.util.ArrayList;
+import java.util.List;
 public class TJMaxx {
 
+	 private List<Item> regularItems; 
+	 private List<OnSaleItem> onSaleItems;
+	 public TJMaxx() {
+		ArrayList<Item> item=new ArrayList<>();
+		ArrayList<OnSaleItem> onSaleItem=new ArrayList<>();
+	 }
+	 
+	 public void addRegularItem(Item item) {
+		 regularItems.add(item);
+	 }
+	 public void addOnSaleItem(OnSaleItem item) {
+		 addOnSaleItem(item);
+	 }
+	 public List<Item> getRegularItems() {
+		 	return regularItems;   
+	 }
+	 public List<OnSaleItem> getOnSaleItems() {
+		 return onSaleItems;  
+	}
+	 public int regularItemsCount() {     
+		 	return -1;	 }
 }
